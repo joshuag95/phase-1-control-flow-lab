29702a7cded6654d8251c0c470a7735fa5c71234
Joshua Greer <jtgreer95@gmail.com>
2022-06-27 15:42:43 -0500

    commit tree 87e61f72add04dddf20ef9c8ec82a734ee6c0f5a

--- index.js
+++ index.js
@@ -1,11 +1,44 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(number){
+  let answer
+  if (number <= 400){
+  answer = "This one is on me!"
 }
+  else if (number > 400 && number < 2001){
+    answer = "That will be twenty bucks."
+  }
+  else if (number > 2000 && number < 2500){
+    answer = "I will gladly take your thirty bucks."
+  }
+  else if (number > 2500){
+    answer = "No can do."
+  }
+  return answer
+}
+function ternaryCheckCity(city){ 
+  
+ const cityMessage = city ? "Ok, sounds good." : "No go.";
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+ return cityMessage
+ 
+ 
+  }
 
 function switchOnCharmFromTip(){
-  // Write your code here!
+const tip = "generous";
+
+let charm;
+
+ switch (tip) {
+  case 'generous':
+    charm = "Thank you so much.";
+    break;
+  case "not as generous":
+    charm = "Thank you.";
+    break;
+  default:
+    console.log("Bye.")
+    break;
+ }
+
+
 }
